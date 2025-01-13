d59ca00cf06428271cc779d0cce6664367bf7724
SebastianRosero7 <jroserom@unicauca.edu.co>
2025-01-13 14:21:51 -0500

    commit inicial

---
+++ src/main/java/com/funeraria/FunerariaApplication.java
@@ -0,0 +1,13 @@
+package com.funeraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunerariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunerariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funeraria/FunerariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funeraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunerariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
